b6016d3f591a0052cbaaf7f4eedb17276883acad
mdoury <maxime.doury@lib.upmc.fr>
2017-08-07 11:39:02 +0200

    Add bash script to publish to GitHub Pages

---
+++ publishToGitHubPages.sh
@@ -0,0 +1,3 @@
+ng b --prod --aot --base-href https://mdoury.github.io/slideshow/
+
+angular-cli-ghpages --repo=https://github.com/mdoury/slideshow.git --name=mdoury --email=maxime.doury@gmail.com --no-silent
